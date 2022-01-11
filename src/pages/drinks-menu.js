e372077523076bef57df49a87b96e3a078a2aac8
Milda Kavaliauskaite <mildak22@gmail.com>
2022-01-11 21:11:00 +0200

    pradėtas formuoti gėrimų puslapis

--- src/pages/drinks-menu.js
+++ src/pages/drinks-menu.js
@@ -1,9 +1,15 @@
 import React from "react";
+import './drinks-menu.scss';
   
 export const DrinksMenu = () => {
   return (
-    <div className="drinksMenu">
-      <h1>vhnfnh</h1>
+    <div className="drinks">
+      <h1>Gėrimai, gėrimai, gėrimai...</h1>
+      <p>Įvairių gaiviųjų gėrimų rūšių yra visa daugybė – limonadas, šaltoji arbata, pieno ir ledų kokteiliai, šalti 
+        kavos gėrimai, trintų vaisių kokteiliai, glotnučiai ir daugybė kitų. Žinoma, klasikinis receptas – vanduo su 
+        citrina ir mėta arba įvairių vaisių ir uogų užpiltinė, veikiausiai, visuomet puikuosis tautiečių mėgstamų 
+        gėrimų sąrašo viršuje, tačiau pasidairykime ir po kitas šalis.
+      </p>
     </div>
   );
 };
