168ef06589874f12f38d2c263a8aa8bc0291be24
Milda Kavaliauskaite <mildak22@gmail.com>
2022-03-08 21:10:44 +0200

    stiliaus pakeitimai

--- src/pages/countrys/amerika.js
+++ src/pages/countrys/amerika.js
@@ -3,11 +3,34 @@ import { Box, Paper, Typography } from '@mui/material';
 import RecipeGridPage from '../recipe-grid-page/recipe-grid-page';
 import '../style.scss'
 
+
+
 const Amerika = () => (
     <Box>
-    <Paper sx={{pt: 30}}>
-        <Typography></Typography>
+    <Paper sx={{pt: 15, backgroundColor: "#A3D2CA"}}>
+        <Typography sx={{ 
+            fontWeight: "bold", 
+            fontSize: "30px",
+            fontFamily: "Josefin Sans",
+            textAlign: "center",
+            mb: "10px" 
+            }}> Amerikos virtuvė</Typography>
+        <Typography sx={{ 
+            fontFamily: "Josefin Sans",
+            fontSize: "17px",
+            ml: "255px", 
+            width: "760px", 
+            textAlign: "justify",
+            mb: "20px" 
+            }}>Tai viena iš vakarietiškos užjūrio virtuvės, kurios tradicijos paplitusios Jungtinėse 
+            Amerikos Valstijose bei jos kaimyninėse šalyse. Šiaurės Amerika garsi savo kulinarijos paveldu, 
+            amerikietiškai maistas gaminamas beribiuose šiuo žemyno plotuose, besidriekiančiuose nuo Aliaskos 
+            iki pat Meksikos ribų. Šios virtuvės maisto gaminimo būdui susiformuoti padėjo valgių paruošimai, 
+            kuriuos naudojo pirmieji emigrantai iš Europos. Tais laikais atvykėliai jau naudojo keptuves bei 
+            puodus, daugelis amerikietiškų patiekalų troškinami arba kepami iki šiol.</Typography>
         <RecipeGridPage/>
+       
+         
     </Paper>
     </Box>
 );
@@ -15,12 +38,5 @@ const Amerika = () => (
 export default Amerika;
 
 
-{/* <div className="text">
-            //     <h1>Amerikos virtuvė</h1>
-            //     <p> Tai viena iš vakarietiškos užjūrio virtuvės, kurios tradicijos paplitusios Jungtinėse Amerikos Valstijose bei
-            //         jos kaimyninėse šalyse. Šiaurės Amerika garsi savo kulinarijos paveldu, amerikietiškai maistas gaminamas
-            //         beribiuose šiuo žemyno plotuose, besidriekiančiuose nuo Aliaskos iki pat Meksikos ribų. Šios virtuvės maisto
-            //         gaminimo būdui susiformuoti padėjo valgių paruošimai, kuriuos naudojo pirmieji emigrantai iš Europos. Tais
-            //         laikais atvykėliai jau naudojo keptuves bei puodus, daugelis amerikietiškų patiekalų troškinami arba kepami iki
-            //         šiol.</p>
-            // </div> */}
+ 
+            
--- src/pages/recipe-grid-page/recipe-grid-page-card.jsx
+++ src/pages/recipe-grid-page/recipe-grid-page-card.jsx
@@ -10,16 +10,36 @@ const RecipeGridPageCard = ({ id, category, title, country, ingredients, instruc
     }, [id]);
   
     return (
-      <Box sx={{ border: "2px solid black" }}>
+      <Box sx={{ 
+       border: "2px solid black", 
+       width: "750px",
+       ml: "265px",
+       mr: "100px",
+       p: "15px"}}>
         <Box>
           <Box onClick={navigateToRecipe}>
           </Box>
           <Box>
-            <Typography sx={{ fontWeight: "bold" }}>{title}</Typography>
-            <Typography>Šalis: {country}</Typography>
-            <Typography>Kategorija: {category}</Typography>
-            <Typography>Ingredientai: {ingredients}</Typography>
-            <Typography>Paruošimas: {instructions}</Typography>
+            <Typography sx={{ 
+              fontWeight: "bold", 
+              fontSize: "30px", 
+              fontFamily: "Josefin Sans" }}>{title}</Typography>
+            <Typography sx={{ 
+              fontSize: "20px", 
+              fontFamily: "Josefin Sans" }}>Šalis: {country}</Typography>
+            <Typography sx={{ 
+              fontSize: "20px", 
+              fontFamily: "Josefin Sans", 
+              pb: "10px"}}>Kategorija: {category}</Typography>
+            <Typography sx={{ 
+              fontSize: "17px", 
+              fontFamily: "Josefin Sans", 
+              textAlign: "justify", 
+              pb:"10px" }}>Ingredientai: {ingredients}</Typography>
+            <Typography sx={{ 
+              fontSize: "17px", 
+              fontFamily: "Josefin Sans", 
+              textAlign: "justify" }}>Paruošimas: {instructions}</Typography>
           </Box>
         </Box>
       </Box>
